d096efe32f2f2addfc45835d3f5655de7d01b260
faisall667 <68485394+faisall667@users.noreply.github.com>
2023-02-05 12:46:44 -0600

    dynamic circles

---
+++ app/app.js
@@ -0,0 +1,86 @@
+let questionArray = [{
+    question:"What does HTML stand for?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What does CSS stand for?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What does JS stand for?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What is block element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+},
+{
+    question:"What inline element?",
+    choice1:"Answer1",
+    choice2:"Answer2",
+    choice3:"Answer3",
+    choice4:"Answer4"
+}
+];
+
+function drawCircles (){
+    let circles = document.getElementById("circles");
+    for (let counter = 1; counter <= questionArray.length; counter++){
+        let newCircle = document.createElement("span");
+        newCircle.classList.add("dot");
+        newCircle.innerHTML = counter;
+        circles.appendChild(newCircle); 
+    }
+ }
+
+
+ drawCircles ()
+
+
