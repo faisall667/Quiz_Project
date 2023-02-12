cd609e45c34a4525e8947ac6e0579cb9459f6fb8
Shujauddin <sadat.shujauddin@gmail.com>
2023-02-12 09:28:43 -0600

    updated app.js and htmlIndex

--- app/app.js
+++ app/app.js
@@ -1,77 +1,37 @@
 
 let questionArray = [{
-    question:"1. What does HTML stand for?",
-    choice1:"Hyper Text Markup Language",
-    choice2:"World Wide Web",
-    choice3:"High Text Markup Language",
-    choice4:"Hyper Text Makeup Language",
-    //correct: "choice1"
-},
-{
-    question:"2. What does CSS stand for?",
-    choice1:"Case Style Sheets",
-    choice2:"Case Sensitive Sheet",
-    choice3:"Cascading Style Sheets",
-    choice4:"Class Style",
-    //correct: "choice3"
-},
-{
-    question:"3. What does JS stand for?",
-    choice1:"Jake Scope",
-    choice2:"Java Script",
-    choice3:"Java",
-    choice4:"Java Scripting"
-    //correct: "choice2"
-},
-{
-    question:"4. What is block element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
+    question:"What does HTML stand for?",
+    choiceA:"Hypertext Markup Language",
+    choiceB:"Cake",
+    choiceC:"Drink",
+    choiceD:"Sleep",
+    correct:"A"
 },
 
 {
-    question:"5. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
+    question:"What does CSS stand for?",
+    choiceA:"Apple",
+    choiceB:"Cascade Style Sheet",
+    choiceC:"Orange",
+    choiceD:"Banana",
+    correct:"B"
 },
 {
-    question:"6. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
+    question:"What does JS stand for?",
+    choiceA:"John Shah",
+    choiceB:"Jams",
+    choiceC:"JavaScript",
+    choiceD:"Java solar",
+    correct:"C"
+    
 },
 {
-    question:"7. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
-},
-{
-    question:"8. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
-},
-{
-    question:"9. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
-},
-{
-    question:"10. What inline element?",
-    choice1:"Answer1",
-    choice2:"Answer2",
-    choice3:"Answer3",
-    choice4:"Answer4"
+    question:"What is block element?",
+    choiceA:"Bob",
+    choiceB:"Boy",
+    choiceC:"Bag",
+    choiceD:"Block-level element",
+    correct:"D"
 }
 
 
@@ -86,30 +46,26 @@ function drawCircles (){
         circles.appendChild(newCircle); 
     }
  }
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+const next = document.getElementById("next");
+
  drawCircles ()
 
- 
- let counter = 0;
- let question = questionArray[counter];
- document.getElementById("question").innerHTML = question.question
- choice1.innerHTML = question.choice1;
- choice2.innerHTML = question.choice2;
- choice3.innerHTML = question.choice3;
- choice4.innerHTML = question.choice4;
- console.log(question);
-
- function nextQuestion() {
-    counter++;
-    question = questionArray[counter].question;
-    document.getElementById("question").innerHTML = question
-}
-
-
- 
-
-
-
-
+const lastQuestion = questionArray.length - 1;
+let runningQuestion = 0;
+let score = 0;
+    let question = questionArray[runningQuestion];
+    document.getElementById("question").innerHTML = question.question
+    choiceA.innerHTML = question.choiceA;
+    choiceB.innerHTML = question.choiceB;
+    choiceC.innerHTML = question.choiceC;
+    choiceD.innerHTML = question.choiceD;
+    console.log (question)
+   
 
     
 
